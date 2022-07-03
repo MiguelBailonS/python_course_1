67e97aff81df901567ac1d2a4112fdc5316c6e37
miguelbailons <pasantia_fw@easymetering.com>
2022-07-03 16:21:04 -0500

    ADD: FStrings-Formatted strings

---
+++ 24_FString.py
@@ -0,0 +1,25 @@
+name = 'Miguel '
+last_name = 'Bailon'
+
+#Introduction to FStrings
+
+#This is going to be an FString
+#Create new strings with interpolation
+#Using FStrings
+
+full_name = f'Mr. {name} {last_name} {"Silva"}'
+print(full_name)
+
+full_name = f'Mr. {name} {last_name} {True}'
+print(full_name)
+
+full_name = f'Mr. {name} {last_name} {1.1}'
+print(full_name)
+
+full_name = f'Mr. {name} {last_name} {2 }'
+print(full_name)
+
+full_name = f'Mr. {name} {last_name} {2*20 }'
+print(full_name)
+
+#This helps a lot
